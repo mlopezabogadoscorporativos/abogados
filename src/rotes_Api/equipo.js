5e47d6277de08d581474bfa82f922e34f2355696
mlopezabogadoscorporativos <mlopezabogadoscorporativos@gmail.com>
2025-05-06 17:12:06 -0500

    actualizacion de imagen

--- src/rotes_Api/equipo.js
+++ src/rotes_Api/equipo.js
@@ -1,6 +1,6 @@
 const equipos = [
     {id: 1, nombre: "MEDARDO LOPEZ", cargo:"Abogado" ,urlImage: "https://res.cloudinary.com/dhxefh3r2/image/upload/v1746559564/qetvl2jzrbqnjbgimo7f.jpg"},
-    {id: 2, nombre: "GLADYS MORETO", cargo:"Abogada" ,urlImage: "https://res.cloudinary.com/dhxefh3r2/image/upload/v1746559626/d16eic4joc7e6bblxigs.jpg"},
+    {id: 2, nombre: "GLADYS MORETO", cargo:"Abogada" ,urlImage: "https://res.cloudinary.com/dhxefh3r2/image/upload/v1746569476/aojcmbrfne8nakhjlv79.jpg"},
     {id: 3, nombre: "DICKSON CHAU", cargo:"Abogado" ,urlImage: "https://res.cloudinary.com/dhxefh3r2/image/upload/v1746559626/d16eic4joc7e6bblxigs.jpg"},
     {id: 4, nombre: "YURI HERNANDEZ", cargo:"Abogada" ,urlImage: "https://res.cloudinary.com/dhxefh3r2/image/upload/v1746559672/iw74pop0csymg0qsmvmd.jpg"},
     {id: 5, nombre: "MELISSA LALUPU", cargo:"Abogado" ,urlImage: "https://res.cloudinary.com/dhxefh3r2/image/upload/v1746559728/w6h23affmud0slzrrfm7.jpg"},
